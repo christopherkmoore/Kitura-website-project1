07b111bfa4ca09fc7b3185dbb54a537e694de7d2
christopherkmoore <christopherkmoore1@gmail.com>
2017-04-02 14:51:39 -0700

    not sure

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,53 +1,31 @@
 import Kitura
-import LoggerAPI 
+import LoggerAPI
 import HeliumLogger
 import KituraStencil
 
 HeliumLogger.use()
 let router = Router()
-router.setDefault(templateEngine: StencilTemplateEngine())
 
-let bios = [
-    "kirk": "My name is James Kirk and I love snakes.",
-    "picard": "My name is Jean-Luc Picard and I'm mad for cats.",
-    "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
-    "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
-    "archer": "My name is Jonathan Archer and beagles are my thing."
-]
+router.setDefault(templateEngine: StencilTemplateEngine())
+router.all("/static", middleware: StaticFileServer())
+
 
 router.get("/") {
     request, response, next in
-    try response.render("home", context: [:])
+    try response.render("blog", context: [:])
     next()
 }
 
-router.get("/staff") {
+router.get("/work") {
     request, response, next in
-    var context = [String: Any]()
-    
-    context["people"] = bios.keys.sorted()
-    
-    try response.render("staff", context: context)
+    try response.render("work", context: [:])
     next()
-    
 }
 
-router.get("/staff/:name") {
+router.get("/about") {
     request, response, next in
-    guard let name = request.parameters["name"] else { return }
-    
-
-    var context = [String: Any]()
-    
-    context["people"] = bios.keys.sorted()
-    
-    if let bio = bios[name] {
-        context["name"] = name
-        context["bio"] = bio
-    }
-    try response.render("staffName", context: context)
+    try response.render("about", context: [:])
     next()
-
 }
 
 router.get("/contact") {
@@ -57,5 +35,4 @@ router.get("/contact") {
 }
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
-Log.info("Haters gun h8")
 Kitura.run()
