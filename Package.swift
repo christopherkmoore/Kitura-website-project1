73cf4966364ff238caf0aafc66e69d76d196f41c
christopherkmoore <christopherkmoore1@gmail.com>
2017-04-05 15:35:58 -0700

    try with new version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
 	.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
 	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
 	.Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1),
-	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",  majorVersion: 0),
+	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",  majorVersion: 1),
 	.Package(url: "https://github.com/IBM-Swift/Kitura-Markdown.git", majorVersion: 0)
 
 	]
